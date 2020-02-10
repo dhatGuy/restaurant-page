8640558d58a67844a25e766f9a0e92495e2d0c50
dhatguy <odunsiolakunbi@gmail.com>
2020-02-10 01:56:49 +0100

    add index page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-alert("Hello")
+import body from './main.js'
+
+document.addEventListener('DOMContentLoaded', (e)=>{
+    document.querySelector('#content').innerHTML = body
+})
+console.log(body)
---
+++ src/main.js
@@ -0,0 +1,16 @@
+let body = 
+    ` <div class="bg-img"></div>
+      <div class="details">
+        <div class="details-heading">
+          <h1>Odunsi's Restaurant</h1>
+        </div>
+        <p class="details-content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non dolore
+          architecto rem! Atque, enim commodi exercitationem corrupti iste illum
+          temporibus eum rem! Corporis dolore assumenda perferendis harum
+          aliquam officia incidunt, dignissimos quo, minus rem, quam consectetur
+          debitis iste! Libero, autem?
+        </p>
+      </div>
+    `
+export default body;
